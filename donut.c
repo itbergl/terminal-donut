7a6f6230ec6e6f1ae0988135c206e072e14044f9
Isaac Bergl <& d:/OneDrive/git/Poker/.venv/Scripts/python.exe d:/OneDrive/git/Poker/main.pyIsaac Bergl>
2021-01-21 03:11:36 +0800

    init

---
+++ donut.c
@@ -0,0 +1,34 @@
+#include "donut.h"
+
+
+
+int generateDonut(SPACE space) {
+
+    //make circle of radius r center (a,b), on xy plane
+
+    //rotate around y=0
+
+
+
+
+
+}
+
+int main(int argc, const char* argv[]) {
+
+    //check command-line arguments
+
+    SPACE space;
+
+    generateDonut(space);
+
+
+
+
+
+
+
+    return 0;
+}
+
+
---
+++ donut.h
@@ -0,0 +1,16 @@
+//includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <strings.h>
+
+
+//data structures
+struct point{
+    int x;
+    int y;
+    int z;
+};
+
+typedef struct point *SPACE;
+
